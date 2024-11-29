b9a08e2539b0415290a49aab1d6728989ca8ef61
Kevin Nicholas Nathanael <108829565+kevvnnn@users.noreply.github.com>
2024-11-29 10:52:24 +0700

    createNewPlayer & addPlayer updates

--- src/game.py
+++ src/game.py
@@ -2,16 +2,17 @@ def initPlayers():
   global PlayerList
   PlayerList = []
 
-def createNewPlayer(name, damage=0, defensePower=0):
+def createNewPlayer(name, damage = 0, defensePower = 0):
   Player = dict(name = name,
-                score = 0,
-                damage = damage,
-                health = 100,
-                defensePower = defensePower,
-                defense = False)
-  PlayerList.append(Player)
+              score = 0,
+              damage = damage,
+              health = 100,
+              defensePower = defensePower,
+              defense = False)
+  return Player
 
 def addPlayer(player):
+  PlayerList.append(player)
 
 
 def removePlayer(name):
