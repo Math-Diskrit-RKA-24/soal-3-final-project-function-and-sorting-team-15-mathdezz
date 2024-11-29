66cb496d76c0dd104ad5f7339cf1e38940834290
Umar Al Faris <umaralfaris13@gmail.com>
2024-11-29 10:05:50 +0700

    def awal
    def awal

--- game.py
+++ game.py
@@ -1,1 +1,20 @@
+def initPlayers():
+
+
+def createNewPlayer(name, damage=0, defensePower=0):
+
+
+def addPlayer(player):
+
+
+def removePlayer(name):
+
+
+def setPlayer(player, key, value):
+
+
+def attackPlayer(attacker:dict, target:dict):
+
+
+def displayMatchResult():
 
